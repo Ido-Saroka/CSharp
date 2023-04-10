a8117eeae83cd30f1402d05779d4951494fbec7b
IdoS <ido.saroka@easyr.co>
2023-04-10 11:32:34 +0300

    Started working xUnit testing project
    * Added xUnit and AutoFixutre (used for mocking0
    * Created dedicated test project "CSharpLibrary.Tests"
    * Started working on tests for "FileComparison" class

---
+++ General Functions/CSharpLibrary.Tests/FileOperations.Tests/FileComparison.Tests.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Security.Cryptography;
+
+namespace CSharpLibrary.Tests.FileOperations.Tests
+{
+  internal class FileComparison
+  {
+
+
+
+    private static HashAlgorithm CreateFileMocking(HashAlgorithm algorithmToUseForMocking)
+    {
+      throw new NotImplementedException();
+    }
+
+
+    public void PerfromFolderComparisonMD5_MockTests()
+    {
+      //Arrange
+
+
+      //Act
+
+
+      //Assert
+    }
+  }
+}
